6527d352d565cf060e119e0adc0c81cfbdff0008
이재영 <jaeyoung.yi@seculetter.com>
2025-01-02 13:26:43 +0900

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by 이재영 on 1/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
