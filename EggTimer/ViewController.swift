c40adbeeb9a9a4f7ac3ce8dfcc6aeb0903c59774
이재영 <jaeyoung.yi@seculetter.com>
2025-01-03 09:13:41 +0900

    81. Setting up the Egg Timer Project and Linking the Storyboard and ViewController

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+    }
+    
 }
 
