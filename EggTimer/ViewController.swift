e329c024d31b5a08865a97d0e473f630322b3091
이재영 <jaeyoung.yi@seculetter.com>
2025-01-03 21:37:55 +0900

    90. Showing the Timer to the User with a Progress View

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     var remaining = 0
@@ -38,18 +40,23 @@ class ViewController: UIViewController {
     func countdown(_ seconds: Int) {
         timer.invalidate()
 
+        titleLabel.text = "How do you like your eggs?"
+        progressBar.progress = 0
         remaining = seconds
         print("\(remaining) seconds")
 
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-            if 0 < self.remaining {
+            if self.progressBar.progress < 1 {
                 self.remaining -= 1
-                print("\(self.remaining) seconds")
+                self.progressBar.progress += 1/Float(seconds)
             }
             else {
+                self.remaining = 0
+                self.progressBar.progress = 1
                 timer.invalidate()
                 self.titleLabel.text = "Done!"
             }
+            print("\(self.remaining) seconds")
         }
     }
     
